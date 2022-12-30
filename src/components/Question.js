737e1d639152d1e9d80c14952bb4f2b6e7357f54
Tate <thandley1234@gmail.com>
2022-12-30 13:26:40 -0700

    commit tree 07cf4b82b57bc60dc341672072a8ab0d726c367f

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  // const Timer = ({ seconds }) => {
+  //   useEffect(() => {
+  //     if (!timeRemaining) return;
+  
+  //     const intervalId = setInterval(() => {
+  //       setTimeRemaining(timeRemaining - 1);
+  //     }, 1000);
+  
 
-  // add useEffect code
+  //     return () => clearInterval(intervalId);
+  //   }, [timeRemaining]);
+  
+  //   return (
+  //     <div>
+  //       {Timer}
+  //     </div>
+  //   );
+  // };
+
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+//-----------------------------
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("this will run after 10 seconds")
+    }, 1000)
+    // return () => clearTimeout(timer);
+    }, []);
+//----------------------------
+  // useEffect(() => {
+  //   const interval = null;
+  //   if (isActive) {
+  //     interval = setInterval(() => {
+  //       setSeconds(seconds => seconds - 1);
+  //     }, 1000);
+  //   } else if (!isActive && seconds !== 0) {
+  //     clearInterval(interval);
+  //   }
+  //   return () => clearInterval(interval);
+  // }, [isActive, seconds]);
 
   const { id, prompt, answers, correctIndex } = question;
 
